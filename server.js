f1a56077903f6bce566aeb4cf87ad1fc6c4b149a
nikolay.savenets <nikolay.savenets@exponential.com>
2023-10-27 13:18:53 +0300

    1

--- server.js
+++ server.js
@@ -70,7 +70,8 @@ bot.on('message', async (msg) => {
             // bot.sendMessage(msg.chat.id, JSON.stringify(data, null, 2));
             savedAction[msg.chat.username] = data
             res_msg = `already exists. rewrite the word? (y/Y)`;
-            res_msg += `\n${response.data.translate}`;
+            // res_msg += `\n${response.data.data.translate}`;
+            res_msg += `\n${JSON.stringify(response.data, null, 2)}`;
         } else if(response.data.status === 7) {
             savedAction[msg.chat.username] = null
             res_msg = `word rewritten.`;
